6ac39de58e017ef6d39e47773a157a12226b666b
manin123the-developer <manisankarreddyravi3@gmail.com>
2024-10-26 14:15:25 +0530

    File update from Local Machine Feature Branch.

--- practice.py
+++ practice.py
@@ -1,9 +1,12 @@
 def count_combinations(pieces,target):
     result=list()
     def find_combination(start,target,c_omb):
-        if target=10: #This is the change from ec2 instance.
-           result.append(tuple(c_omb))
+        #if target=10: #This is the change from ec2 instance.
+         if target == 0:  #This is the update from the Local Machine.
+            result.append(tuple(c_omb))
            return
+
+
         if target<0:
             return
         for i in range(start,len(pieces)):
