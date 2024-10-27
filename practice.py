fc9cce8ed856ba24d2c1695a800a8396c9a73e1a
manin123the-developer <manisankarreddyravi3@gmail.com>
2024-10-27 17:15:01 +0530

    Mani

--- practice.py
+++ practice.py
@@ -1,16 +1,9 @@
 def count_combinations(pieces,target):
     result=list()
     def find_combination(start,target,c_omb):
-        #if target=10: #This is the change from ec2 instance.
-         if target == 0:  #This is the update from the Local Machine.
-            result.append(tuple(c_omb))
-
-        if target=10: #This is the change from ec2 instance.
-            # This is the second change from ec2
+        if target==0: #Mani
            result.append(tuple(c_omb))
            return
-
-
         if target<0:
             return
         for i in range(start,len(pieces)):
